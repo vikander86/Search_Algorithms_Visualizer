2a42c6e4ac3d4b1a0b3bcae07c4383b649ce570f
vikander86 <bager86@gmail.com>
2024-02-06 12:46:04 +0100

    Refractored EightPuzzle gui code

--- gui_test.py
+++ gui_test.py
@@ -3,10 +3,10 @@ from customtkinter import *
 from python_files.algorithms import *
 from python_files.containers import *
 import threading
-import theme
 
-theme_path = os.path.expanduser("theme/Hades.json")
+theme_path = "C:\\Users\\bager\\OneDrive\\Projects\\Search_algorithms_visualizer\\theme\\Hades.json"
 set_default_color_theme(theme_path)
+
 """
 init states for Eight Puzzle
 """
@@ -22,11 +22,12 @@ class GUI(CTk):
         """
         super().__init__(*args, **kwargs, )
         self.geometry("1000x800")
+        self.title("Search Algorithms Visualizer")
         
         self.grid_columnconfigure((0,1,2,3,4), weight=1, uniform="a")
         self.grid_rowconfigure(0,weight=1, uniform="a")
-        self.grid_rowconfigure(4,weight=2, uniform="a")
-        self.grid_rowconfigure((1,2,3),weight=3, uniform="a")
+        self.grid_rowconfigure(4,weight=1, uniform="a")
+        self.grid_rowconfigure((1,2,3),weight=4, uniform="a")
         
         """
         Frame work for window
@@ -137,11 +138,8 @@ class GUI(CTk):
         self.reset_button = CTkButton(result_frame, text="RESET", width=100, height=50, corner_radius=50)
         self.reset_button.grid(row=0, column=1, padx=(2.5,2.5), pady=10, sticky="w")
         
-        self.path_cost_label = CTkLabel(result_frame, text="Path Length: ")
-        self.path_cost_label.grid(row=0, column=2, padx=(2.5,2.5), pady=10, sticky="w")
-        
-        self.explored_label = CTkLabel(result_frame, text="Nodes Explored: ")
-        self.explored_label.grid(row=0, column=3, padx=((2.5,5)), pady=10, sticky="w")
+        self.result_of_solution = CTkLabel(result_frame, text="")
+        self.result_of_solution.grid(row=0, column=2, columnspan=2, padx=(2.5,2.5), pady=5)
         
         """
         INITIAL STATE, GOAL STATE, SOLUTION
@@ -178,6 +176,13 @@ class GUI(CTk):
         setup_thread = threading.Thread(target=self.EightPuzzle_gui)        
         setup_thread.start()
     
+    def validate_eightpuzzle_input(self):
+        # throw a validation check for each entry, see callback()
+        for i, row_entries in enumerate(self.entries):
+            for j, entry in enumerate(row_entries):
+                validate_cmd = (self.register(lambda P, i=i, j=j: self.callback(P, i, j)), "%P")
+                entry.configure(validate='key', validatecommand=validate_cmd)
+    
     def callback(self, P, row, col):
         # Return if input is not a digit, a sequence, or the number 9
         if not P.isdigit() or len(P) > 1 or P == "9":
@@ -199,7 +204,7 @@ class GUI(CTk):
                 entry.configure(validate="none")
                 entry.delete(0,"end")
                 entry.configure(validate="all")
-
+    
     def update_state_eightpuzzle(self, solution, actions, step_index=0):
         if step_index >=len(solution):
             return
@@ -246,7 +251,9 @@ class GUI(CTk):
         self.solution_action.configure(font=(None, 25))
         
         self.solution_progress.set(self.progress)
-        
+        self.solution_progress.grid(row=3, column=0, padx=5, pady=2.5)
+
+        # Get user input
         array = []
         for row in self.entries:
             row_list = []
@@ -254,28 +261,31 @@ class GUI(CTk):
                 number = column.get()
                 row_list.append(int(number))
             array.append(row_list)
-   
+
+        # Setting up the Eight Puzzle problem
         problem = EightProblem(array)
         solver = Search_Algorithms(problem)
-        get_algorithm = self.selected_algorithm.get()
-        assign_algorithm = getattr(solver, get_algorithm, None)
-        result = assign_algorithm()
-        solution = problem.reverse_steps(result)
-        actions = problem.reverse_actions(result)
+        get_algorithm = self.selected_algorithm.get()           # Get the StringVar from the radiobuttons. 
+        assign_algorithm = getattr(solver, get_algorithm, None) # Match the StringVar with the algorithm method in ./Python_files/Algorithms.py
+        result = assign_algorithm()                             # Return result (final node)
+        solution = problem.reverse_steps(result)                # Return a list with initial state to goal state
+        actions = problem.reverse_actions(result)               # Return a list with actions based on the empty tile
         actions.append("SUCCESS")
 
-        self.solution_progress.grid(row=3, column=0, padx=5, pady=2.5)
+        # 
         self.update_state_eightpuzzle(solution, actions, 0)
         
-        self.after(0, lambda: self.path_cost_label.configure(text=f"Path Length: {len(solution)}"))
-        self.after(0, lambda: self.explored_label.configure(text=f"Nodes Explored: {result.explored}"))
+        self.after(0, lambda: self.result_of_solution.configure(text=f"Path Length: {len(solution)}    Nodes Explored: {result.explored}"))
+        # self.after(0, lambda: self.explored_label.configure(text=f""))
         self.after(0, lambda: self.solution_banner.configure(text=f"Solution\n\n{get_algorithm}"))
-   
+
     """ 
     Setting up the Eight Puzzle UI
     """
     def EightPuzzle_gui(self):
-
+        """
+        Initialize buttons - solve and reset
+        """
         self.reset_button.configure(command=self.reset_board)
         self.solve_button.configure(command=self.solve_eightpuzzle)
 
@@ -285,84 +295,53 @@ class GUI(CTk):
         self.banner.configure(text="The Eight Puzzle.\nType in integers between 0-8 (0 representing the empty tile).\nPress solve to see magic.")
         
         """
-        Initialize grid, button input for initial state
-        """
+        FRAME instantiation
         
-        self.initial_frame_puzzle = CTkFrame(self.initial_frame, border_width=0, fg_color="transparent")
-        self.initial_frame_puzzle.grid(row=1, column=0, sticky="nswe", padx=5, pady=5)
-        self.initial_frame_puzzle.grid_rowconfigure((0,2), weight=2, uniform="a")
-        self.initial_frame_puzzle.grid_rowconfigure(1, weight=1, uniform="a")
-        self.initial_frame_puzzle.grid_columnconfigure((0,2), weight=2, uniform="a")
-        self.initial_frame_puzzle.grid_columnconfigure(1, weight=1, uniform="a")
+        Return a frame with specific configurations for the Eight Puzzle
+        """
+        def frame_builder(parent_frame):
+            frame = CTkFrame(parent_frame, border_width=0, fg_color="transparent")
+            frame.grid(row=1, column=0, sticky="nswe", padx=5, pady=5)
+            frame.grid_rowconfigure((0,2), weight=2, uniform="a")
+            frame.grid_rowconfigure(1, weight=1, uniform="a")
+            frame.grid_columnconfigure((0,2), weight=2, uniform="a")
+            frame.grid_columnconfigure(1, weight=1, uniform="a")
+            return frame
         
         """
-        Logic for creating Eight Puzzle board with entries.
+        Building the frames
         """
-
-        # Create entry widgets for input
-        for i in range(3):
-            row_entries = []
-            for j in range(3):
-                entry = CTkEntry(self.initial_frame_puzzle, width=30, height=30, validate="all")
-                entry.grid(row=i, column=j, sticky="e" if j == 0 else "w" if j == 2 else "") # Added logic to make the board nice and neat
-                row_entries.append(entry)
-            self.entries.append(row_entries)
+        self.initial_frame_puzzle = frame_builder(self.initial_frame)
+        self.goal_frame_puzzle = frame_builder(self.goal_frame)
+        self.solution_frame_output = frame_builder(self.solution_frame)
         
-        # Logic for validating inputs (see callback())
-        for i, row_entries in enumerate(self.entries):
-            for j, entry in enumerate(row_entries):
-                # setting up validation cmd, 
-                validate_cmd = (self.register(lambda P, i=i, j=j: self.callback(P, i, j)), "%P")
-                entry.configure(validate='key', validatecommand=validate_cmd)
- 
         """
-        Load goal state - set to default, make adjustable later
+        WIDGET instantiation
+        
+        Returns list with widgets for Eight Puzzle board - input, goal and solution.
         """
-        self.goal_frame_puzzle = CTkFrame(self.goal_frame, border_width=0, fg_color="transparent")
-        self.goal_frame_puzzle.grid(row=1, column=0, sticky="nswe", padx=5, pady=5)
-        self.goal_frame_puzzle.grid_rowconfigure((0,2), weight=2, uniform="a")
-        self.goal_frame_puzzle.grid_rowconfigure(1, weight=1, uniform="a")
-        self.goal_frame_puzzle.grid_columnconfigure((0,2), weight=2, uniform="a")
-        self.goal_frame_puzzle.grid_columnconfigure(1, weight=1, uniform="a")
+        def create_board(type="", frame_size=30, font_size=20):
+            widget_list = []
+            for i in range(3):
+                row_entries = []
+                for j in range(3):
+                    if type == "entries": new_widget = CTkEntry(self.initial_frame_puzzle, width=frame_size, height=frame_size, validate="all")
+                    elif type == "solution_labels": new_widget = CTkLabel(self.solution_frame_output, text="", width=frame_size, height=frame_size, font=(None, font_size), fg_color="black", corner_radius=5)
+                    elif type == "goal_labels": new_widget = CTkLabel(self.goal_frame_puzzle, text=f"{goal_state[i][j]}",
+                                                                    width=frame_size, height=frame_size, font=(None, font_size), fg_color="black", corner_radius=5)
+                    new_widget.grid(row=i, column=j, sticky="e" if j == 0 else "w" if j == 2 else "") # Added logic to make the board nice and neat
+                    row_entries.append(new_widget)
+                widget_list.append(row_entries)
+            return widget_list
         
-        
-        # create a list of goal labels
-        self.goal_representation = []
-        
-        for i in range(3):
-            row_entries = []
-            for j in range(3):
-                goal_tile = CTkLabel(self.goal_frame_puzzle, text=f"{goal_state[i][j]}", 
-                                     width=30, height=30, font=(None, 20), fg_color="black", corner_radius=5)
-                if j == 0:
-                    sticky_value = "e"
-                elif j == 2:
-                    sticky_value = "w"
-                else:
-                    sticky_value = ""
-                goal_tile.grid(row=i, column=j, sticky=sticky_value)
-                row_entries.append(goal_tile)
-            self.goal_representation.append(row_entries)
-            
         """
-        SOLUTION FRAME
-        """      
-        self.solution_frame_output = CTkFrame(self.solution_frame, border_width=0, fg_color="transparent")
-        self.solution_frame_output.grid(row=1, column=0, sticky="nswe", padx=5, pady=5)
-        self.solution_frame_output.grid_rowconfigure((0,2), weight=2, uniform="a")
-        self.solution_frame_output.grid_rowconfigure(1, weight=1, uniform="a")
-        self.solution_frame_output.grid_columnconfigure((0,2), weight=2, uniform="a")
-        self.solution_frame_output.grid_columnconfigure(1, weight=1, uniform="a")
-
-        # create a list of goal labels
-        for i in range(3):
-            row_entries = []
-            for j in range(3):
-                solution_tile = CTkLabel(self.solution_frame_output, text="", width=50, height=50, font=(None, 40), fg_color="black", corner_radius=5)
-                sticky_value = "e" if j==0 else "w" if j == 2 else ""
-                solution_tile.grid(row=i, column=j, sticky=sticky_value)
-                row_entries.append(solution_tile)
-            self.solution_representation.append(row_entries)
+        Call functions set up problem
+        """
+        self.entries = create_board("entries")
+        self.goal_representation = create_board("goal_labels")
+        self.solution_representation = create_board("solution_labels", 50,40)
+        self.validate_eightpuzzle_input()
+        
         
 
 def main():
